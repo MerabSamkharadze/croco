a8793c31e3e2233465d1f409445acd336e922179
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-12 21:17:05 +0400

    add needed components : footer, navigation, table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NavigtionComponent } from './navigtion/navigtion.component';
+import { TableComponent } from './table/table.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    NavigtionComponent,
+    TableComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/navigtion/navigtion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigtion',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigtion.component.html',
+  styleUrl: './navigtion.component.css'
+})
+export class NavigtionComponent {
+
+}
